cc1c41267173f6765774114fcb70d628078e701d
digidoll <digi-doll@hotmail.com>
2021-12-12 12:22:35 -0500

    undo

--- 51/index.php
+++ 51/index.php
@@ -4,7 +4,7 @@
  require_once( 'assets/include/header.php' );
  ?>
 
-	<h1> index THIS IS A TEST
+	<h1> index
 	<span> homepage </span></h1>
     <p><b>bold</b> <i>italic</i> <u>underline</u> <s>strike</s> <a href="#">link</a> "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac feugiat velit, varius malesuada dui. Cras dictum eros sit amet consequat vulputate. Quisque purus turpis, hendrerit non pharetra vitae, tempor tempor orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce placerat, turpis non aliquet sollicitudin, risus mi tristique felis, id scelerisque turpis lorem quis magna. In lacinia augue enim, nec porta velit dignissim et. Vestibulum rutrum non arcu elementum condimentum. Ut accumsan a risus sit amet dictum. Etiam id dictum arcu.<p>
 
